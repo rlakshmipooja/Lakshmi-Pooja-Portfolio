3c41f0f8f9f710101948d7b4ee3ca8183493a35f
Lakshmi Pooja Ramesh <lakshmipooja.ramesh@gmail.com>
2024-08-28 12:54:55 +0530

    feat: navigation in header

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 import './Nav.css'
 
